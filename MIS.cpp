2319e7bdd0463aa3fa1193ac00a8e0eb9d0ff92b
“Sissisnothere” <sissisnothere@users.noreply.github.com>
2016-03-20 12:16:44 -0400

    store set in decreasing order

--- MIS.cpp
+++ MIS.cpp
@@ -8,24 +8,52 @@
 #include <fstream>
 
 using namespace std;
- 
+
+/* store set in decreasing order */
+struct classcomp {
+  bool operator() (const int& lhs, const int& rhs) const
+  {return lhs>rhs;}
+};
+
 void compSetup() {
 	
 	ifstream origFile("original.txt");
 	
-	map<string,int > origMap; 	// check # of duplicates
+	vector<string> fileKey;
+	map<string,int > dupMap; 	// check # of duplicates
 	map<string,int>::iterator it;	// checking duplicate
-	set<string> set; //used for geting duplicate orders
-	string lineTmp;
-	while(getline(origFile,lineTmp)){
-		it = origMap.find(lineTmp);
-		if (it != origMap.end()) {
+	set<int, classcomp> dupSet; //used for geting duplicate orders
+	string line;
+	
+	/* read line by line, and store in the vector for future use, also store number of duplicate line in map, value is duplicate */ 
+	while(getline(origFile,line)){
+		fileKey.push_back(line);
+		
+		it = dupMap.find(line);
+		//cout<< " key " << it->first << endl; 
+		//cout<< " value " << it->second << endl; 
+		if (it != dupMap.end()) {
 			it->second++;
+		//	cout << "increasing value" << endl;
 		}
 		else {
-			origMap.insert (pair<string,int>(lineTmp,1) );
+			dupMap.insert (pair<string,int>(line,1) );
+		//	cout << "create a new entry " << endl;
 		}
-    }
+	}
+	
+	for(it = dupMap.begin(); it != dupMap.end(); it++)
+	{
+		cout << "key: " << it->first << endl;
+		cout << "value " << it->second << endl; 
+		dupSet.insert(it->second); 	//store repeating value in order
+	} 
+	
+	set<int>::iterator setIt;
+	for(setIt=dupSet.begin(); setIt!=dupSet.end(); ++setIt)
+	{
+		cout << "set is " <<  *setIt << endl;
+	}
 	 /* write to file */
 	ofstream outFile("cout.txt");
 	// streambuf *coutbuffer = cout.rdbuf();
