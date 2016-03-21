ad0c1ba0960b39415d00f0523fc52af82e9042b6
“Sissisnothere” <sissisnothere@users.noreply.github.com>
2016-03-20 22:29:37 -0400

    record progress on compressing

--- MIS.cpp
+++ MIS.cpp
@@ -15,15 +15,21 @@ struct classcomp {
   {return lhs>rhs;}
 };
 
+vector<string> fileKey;
+map<string,int > dupMap; 	// check # of duplicates
+map<string,int>::iterator it;	// checking duplicate
+//vector<pair<string, int > > mapForSort;
+map<string,int > dictionary;	// int is for index value in the dictionary
+int choice = 0; // 1.RLE 2.bitmask 3. 1 bit mismatch, 4. 2consecutive mismatch 5. 2bit mismatch 6. DirectMatch 7. Original
+string lastLine;
+
 void compSetup() {
 	
 	ifstream origFile("original.txt");
+
+ 	set<int, classcomp> dupSet; //used for geting duplicate orders
+ 	set<int>::iterator setIt;
 	
-	vector<string> fileKey;
-	map<string,int > dupMap; 	// check # of duplicates
-	map<string,int>::iterator it;	// checking duplicate
-	set<int, classcomp> dupSet; //used for geting duplicate orders
-	string line;
 	
 	/* read line by line, and store in the vector for future use, also store number of duplicate line in map, value is duplicate */ 
 	while(getline(origFile,line)){
@@ -44,16 +50,43 @@ void compSetup() {
 	
 	for(it = dupMap.begin(); it != dupMap.end(); it++)
 	{
-		cout << "key: " << it->first << endl;
-		cout << "value " << it->second << endl; 
+		//cout << "key: " << it->first << endl;
+		//cout << "value " << it->second << endl; 
 		dupSet.insert(it->second); 	//store repeating value in order
 	} 
 	
-	set<int>::iterator setIt;
-	for(setIt=dupSet.begin(); setIt!=dupSet.end(); ++setIt)
+	/* create 8 entries dictionary*/
+	
+	int dictSize = 0;
+	
+	for(setIt=dupSet.begin(); setIt!=dupSet.end() ; ++setIt)
 	{
-		cout << "set is " <<  *setIt << endl;
+		for(it = dupMap.begin(); it != dupMap.end() && dictSize <8; it++)
+		{		
+			//cout << *setIt << " is " << it->second << endl;
+			if(it->second == *setIt) {
+				//mapForSort.push_back(pair<string, int>(it->first,dictSize));
+				dictionary.insert(pair<string,int >(it->first,dictSize));
+				//cout << "pushing " << it->first << endl;
+				dictSize++;
+			}	
+		} 
+		if(dictSize == 8)
+			break;
+		
 	}
+	
+	// for(int i=0;i<dictSize;i++) {
+// 		cout << "mapForSort " << mapForSort.at(i).first << " and " << i << endl;
+// 		dictionary.insert(pair<string,int >(mapForSort.at(i).first,i));
+// 	}
+//
+	for(auto iterator = dictionary.begin() ; iterator != dictionary.end() ; ++iterator )
+	{
+		cout << iterator->first<< endl;
+		cout << iterator->second << endl;
+	}
+	
 	 /* write to file */
 	ofstream outFile("cout.txt");
 	// streambuf *coutbuffer = cout.rdbuf();
@@ -67,13 +100,56 @@ void  depreSetup() {
 }
 
 void depression() {
-	depreSetup();
+	
 	cout << "depressing " << endl;
 }
 
 void compression() {
-	compSetup();
-	cout << "compressing " << endl;
+	
+	cout << "start compressing: " << endl;
+	//vector<string>::iterator vecIt;
+	stringstream ss;
+	string pre;
+	string format;
+	string line = "";
+	string lastFormat = "";
+	int RLEindex = 0;
+	string lastPre = "";
+	
+	for(int i = 0 ; i < fileKey.size(); i++) {
+		it = dictionary.find(fileKey.at(i));
+		//vecIt = find(dictionary.begin()->first, dictionary.end()->first, fileKey.at(i));
+		if(it != dictionary.end())
+			choice = 1;
+		else if(lastLine.compare(fileKey.at(i))) {
+			
+		}
+		
+		switch(choice){
+			case 1:		/*RLE*/ 			pre = "000";
+											break;
+			case 2: 	/*bistmask*/		pre = "001";
+											break;
+			case 3: 	/*1 bit*/			pre = "010";
+											break;
+			case 4: 	/*2bit consecutive*/pre = "011";
+											break;
+			case 5: 	/*2bit anywhere*/	pre = "100";
+											
+											break;
+			case 6: 	/*Direct Match*/	pre = "101";
+											format = bitset<3>(it->second).to_string();
+											lastFormate = pre + format;
+											ss<< pre << format;
+											break;
+			case 7: 	/*original*/		pre = "111";
+											formate = fileKey.at(i)；
+											lastFormate = pre + format;
+											ss<< pre << format;	
+											break;
+		}
+		lastLine = fileKey.at(i);
+	}
 }
 
 int main(int argc, char* argv[]) { 
@@ -82,18 +158,18 @@ int main(int argc, char* argv[]) {
 	else
 	{
 		if(atoi(argv[1]) == 1){
+			compSetup();
 			compression();
 		}
 		else if(atoi(argv[1]) == 2) {
+			depreSetup();
 			depression();
 		}
 		else {
 			cout << "invild argement "<< atoi(argv[1]) <<endl;
 		}
 	}
-
-    
-
+	
 	return 0;
 }
 	
